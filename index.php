d15c2e23502649f5856288713935c8a890292fa3
badiuszaman <111489318+bedxiomi@users.noreply.github.com>
2022-09-12 12:09:52 +0700

    menambahkan modal

--- index.php
+++ index.php
@@ -28,62 +28,228 @@ include ("config/config.php");
                 <div class="row justify-content-center">
                     <div class="col-md-10">
                         <div class="mb-3">
-                            <a href="kelola/tambah.php" type="button" class="btn btn-primary">Tambah</a>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                                Tambah
+                            </button>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Tambah Data Mahasiswa</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="kelola/prosestambah.php" method="post">
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Masukkan
+                                                        NIM</label>
+                                                    <input type="text" class="form-control" id="nim" name="nim">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Masukkan
+                                                        Nama</label>
+                                                    <input type="text" class="form-control" id="nama" name="nama" ">
+                                                </div>
+                                                <div class=" mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Masukkan
+                                                        Jenis Kelamin</label>
+                                                    <select class="form-control" id="jk" name="jk">
 
-                            <table class="table table-hover table-bordered border-primary mt-3">
-                                <thead class="table-dark">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>NIM</th>
-                                        <th>Nama Mahasiswa</th>
-                                        <th>Jenis Kelamin</th>
-                                        <th>No. Telp</th>
-                                        <th>Alamat</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
+                                                        <option value="Laki-Laki">Laki-Laki</option>
+                                                        <option value="Perempuan">Perempuan</option>
+                                                    </select>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Masukkan
+                                                        No.Telp</label>
+                                                    <input type="text" class="form-control" id="telp" name="telp">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Masukkan
+                                                        Alamat</label>
+                                                    <div class="form-floating">
+
+                                                        <textarea class="form-control col-sm" id="alamat"
+                                                            name="alamat"></textarea>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Tutup</button>
+                                            <button type='submit' class='btn btn-primary me-2'>Tambah</button>
+
+                                        </div>
+                                        </form>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <table class="table table-hover table-bordered border-primary mt-3">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>No</th>
+                                    <th>NIM</th>
+                                    <th>Nama Mahasiswa</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>No. Telp</th>
+                                    <th>Alamat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                                     $query="SELECT * FROM mahasiswa";
                                     $sql = mysqli_query($konek, $query);
                                     $no = 1;
                                     while($data = mysqli_fetch_array($sql)){
                                       ?>
-                                    <tr>
-                                        <th scope="row"><?php echo $no; ?></th>
-                                        <td><?php echo $data['nim']; ?></td>
-                                        <td><?php echo $data['nama']; ?></td>
-                                        <td><?php echo $data['jk']; ?></td>
-                                        <td><?php echo $data['telp']; ?></td>
-                                        <td><?php echo $data['alamat']; ?></td>
-                                        <div class="justify-content-center">
+                                <tr>
+                                    <th scope="row"><?php echo $no; ?></th>
+                                    <td><?php echo $data['nim']; ?></td>
+                                    <td><?php echo $data['nama']; ?></td>
+                                    <td><?php echo $data['jk']; ?></td>
+                                    <td><?php echo $data['telp']; ?></td>
+                                    <td><?php echo $data['alamat']; ?></td>
+                                    <div class="justify-content-center">
 
-                                            <td>
+                                        <td>
+                                            <a type="button" class="btn btn-warning me-1" data-bs-toggle="modal"
+                                                data-bs-target="#modalEdit<?= $data['nim']; ?>">
+                                                Edit
+                                            </a>
+                                            <div class="modal fade" id="modalEdit<?= $data['nim']; ?>" tabindex="-1"
+                                                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-scrollable">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="staticBackdropLabel">Edit Data
+                                                                Mahasiswa
+                                                            </h5>
+                                                            <button type="button" class="btn-close"
+                                                                data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form action="kelola/prosesedit.php" method="post">
+                                                                <div class="mb-3">
+                                                                    <label for="exampleFormControlInput1"
+                                                                        class="form-label">Masukkan
+                                                                        Nama</label>
+                                                                    <input type="text" class="form-control" id="nama"
+                                                                        name="nama" value="<?php echo $data['nama'] ?>">
+                                                                </div>
 
-                                                <a href="kelola/edit.php?nim=<?php echo $data['nim']; ?>"
-                                                    class="btn btn-warning me-3 mb-2">Edit</a>
-                                                <form class="d-inline" action="kelola/hapus.php" method="post">
-                                                    <input type="hidden" name="nim" value="<?= $data['nim']; ?>">
-                                                    <button class="btn btn-danger">Hapus</button>
-                                                </form>
-                                            </td>
+                                                                <div class=" mb-3">
+                                                                    <label for="exampleFormControlInput1"
+                                                                        class="form-label">Masukkan
+                                                                        Jenis Kelamin</label>
+                                                                    <select class="form-control" id="jk" name="jk">
+                                                                        <option selected value="
+                                                                            <?php echo $data['jk'] ?>">
+                                                                        </option>
+                                                                        <option value="Laki-Laki">Laki-Laki</option>
+                                                                        <option value="Perempuan">Perempuan</option>
+                                                                    </select>
+                                                                </div>
+                                                                <div class="mb-3">
+                                                                    <label for="exampleFormControlInput1"
+                                                                        class="form-label">Masukkan
+                                                                        No.Telp</label>
+                                                                    <input type="text" class="form-control" id="telp"
+                                                                        name="telp" value="<?php echo $data['telp'] ?>">
+                                                                </div>
+                                                                <div class="mb-3">
+                                                                    <label for="exampleFormControlInput1"
+                                                                        class="form-label">Masukkan
+                                                                        Alamat</label>
+                                                                    <div class="form-floating">
+
+                                                                        <textarea class="form-control col-sm"
+                                                                            id="alamat"
+                                                                            name="alamat"><?php echo $data['alamat'] ?></textarea>
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary"
+                                                                data-bs-dismiss="modal">Tutup</button>
+                                                            <!-- <input type="text" name="nim" value="<?= $data['nim']; ?>"> -->
+                                                            <button type='submit' class='btn btn-warning me-2'>Edit
+                                                                Data</button>
+
+                                                        </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    </div>
+
+
+
+
+
+
+                                    <a type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#modalHapus<?= $data['nim']; ?>">
+                                        Hapus
+                                    </a>
+                                    <div class="modal fade" id="modalHapus<?= $data['nim']; ?>"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                                        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="staticBackdropLabel">Hapus Data
+                                                        Mahasiswa
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Apakah Anda
+                                                    Ingin Menghapus Data berikut?
+                                                    <br>
+                                                    Nama : <?= $data['nama']; ?>.
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Tutup</button>
+                                                    <form class="d-inline" action="kelola/hapus.php" method="post">
+                                                        <!-- <input type="text" name="nim" 
+                                                                    value="<?= $data['nim']; ?>"> -->
+                                                        <button class="btn btn-danger">Hapus</button>
+                                                    </form>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </tr>
-                                    <?php
+                                    </div>
+                                    </td>
+                    </div>
+                    </tr>
+                    <?php
                                                 $no++;
                                     }
                                     ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                    </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
+        </div>
     </section>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js">
+
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
--- proseslogin.php
+++ proseslogin.php
@@ -1,17 +1,18 @@
-<?php
-if(isset($_POST['btnlogin'])){
-    $username= $_POST['username'];
-    $password = $_POST['password'];
+<?php 
 
-    if($username == "admin@gmail"){
-        if($password == "admin"){
-        // login sukses
-        header('location:index.php');
-    } else {
-        // gagal
-        header('location:login.php');
-    }}
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+ 
+$login = mysql_query("select * from user where username='$username' and password='$password'");
+$cek = mysql_num_rows($login);
+ 
+if($cek > 0){
+	session_start();
+	$_SESSION['username'] = $username;
+	$_SESSION['status'] = "login";
+	header("location:admin/index.php");
+}else{
+	header("location:index.php");	
 }
-
-
+ 
 ?>
